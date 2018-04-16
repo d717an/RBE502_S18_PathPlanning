1c50d9c420e55679b0791d5b61d33a1fbdabb797
Tri Khuu <kktri94@gmail.com>
2018-04-16 13:07:50 -0400

    Added obstacles generation

---
+++ generate_env.m
@@ -0,0 +1,72 @@
+function generate_env()
+clear;
+clc;
+close all;
+
+figure 
+hold on
+axis equal
+grid on
+axis([0 500 0 500])
+
+[rect_list, tri_list] = get_user_input();
+
+
+end
+
+% Function to add user-defined obstacles
+% The only supported obstacles are triangles and rectangles
+% The function return 2 lists, a list of rectangles and a list of rectangle
+% Each item in the list is a list of lines that define the obstacle
+% Specifically:
+% - an item in the rectangle list is a list of 4 lines
+% - an iten in the triangle list is a list of 3 lines
+function [rect_list, tri_list] = get_user_input()
+
+option_msg = 'Please choose an option (default rectangle):\n1. Add a rectangle\n2. Add a triangle\n3. Done\n=> ';
+
+rect_list = [];
+tri_list = [];
+
+while 1
+    option = input(option_msg);
+    if option == 1
+        disp('Add a rectangle');
+        
+        rect_msg_1 = 'Enter bottom left corner x coordinate => ';
+        rect_msg_2 = 'Enter bottom left corner y coordinate => ';
+        rect_msg_3 = 'Enter length in x => ';
+        rect_msg_4 = 'Enter length in y => ';
+        
+        rect_x      = input(rect_msg_1);
+        rect_y      = input(rect_msg_2);
+        rect_width  = input(rect_msg_3);
+        rect_height = input(rect_msg_4);
+        
+        [l1, l2, l3, l4] = draw_rect(rect_x, rect_y, rect_width, rect_height);
+        rect_list = [rect_list; [l1, l2, l3, l4]];
+        
+    elseif option == 2
+        disp('Add a triangle');
+        
+        tri_msg_1 = 'Enter bottom left corner x coordinate => ';
+        tri_msg_2 = 'Enter bottom left corner y coordinate => ';
+        tri_msg_3 = 'Enter bottom length  => ';
+        tri_msg_4 = 'Enter left length => ';
+        tri_msg_5 = 'Enter angle => ';
+        
+        tri_x           = input(tri_msg_1);
+        tri_y           = input(tri_msg_2);
+        tri_bot_len     = input(tri_msg_3);
+        tri_left_len    = input(tri_msg_4);
+        tri_angle       = input(tri_msg_5);
+        
+        [l1, l2, l3] = draw_triangle(tri_x, tri_y, tri_bot_len, tri_angle, tri_left_len);
+        tri_list = [tri_list; [l1, l2, l3]];
+    else
+        break
+    end
+end
+
+end
+
